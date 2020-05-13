595e94f8f23553b02778c757c95749c2578994d9
Andres Ramirez <andresramirezz@hotmail.com>
2020-05-12 23:03:00 -0400

    Add ArrayCopyTest.java example
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/beginningJava8Fundamentals/arrays/ArrayCopyTest.java
@@ -0,0 +1,56 @@
+package com.beginningJava8Fundamentals.arrays;
+
+import java.util.Arrays;
+
+/**
+ * @author andresr
+ * This code demonstrates how to copy an array using a for loop and the 
+ * System.arraycopy() method.
+ */
+public class ArrayCopyTest {
+
+	public static void main(String[] args) {
+		// Have an array with 5 elements 
+		int[] data = {1, 2, 3, 4, 5 };
+		
+		// Expand the data array to 7 elements 
+		int[] eData = expandArray(data, 7);
+		
+		// Truncate the data array to 3 elements 
+		int[] tData = expandArray(data, 3);
+		
+		System.out.println("Using for-loop..."); 
+		System.out.println("Original Array: " + Arrays.toString(data)); 
+		System.out.println("Expanded Array: " + Arrays.toString(eData)); 
+		System.out.println("Truncated Array: " + Arrays.toString(tData));
+		
+		// Copy data array to new arrays
+		eData = new int[9];
+		tData = new int[2]; 
+		System.arraycopy(data, 0, eData, 0, 5); 
+		System.arraycopy(data, 0, tData, 0, 2);
+		
+		System.out.println("Using System.arraycopy() method..."); 
+		System.out.println("Original Array: " + Arrays.toString(data)); 
+		System.out.println("Expanded Array: " + Arrays.toString(eData)); 
+		System.out.println("Truncated Array: " + Arrays.toString(tData));
+	}
+		
+		// Uses a for-loop to copy an array
+		public static int[] expandArray(int[] oldArray, int newLength) {
+			int originalLength = oldArray.length; 
+			int[] newArray = new int[newLength]; 
+			int elementsToCopy = 0;
+			
+			if (originalLength > newLength) { 
+				elementsToCopy = newLength;
+			}
+			else {
+				elementsToCopy = originalLength;
+			}
+			for (int i = 0; i < elementsToCopy; i++) { 
+				newArray[i] = oldArray[i];
+			}
+			return newArray;
+		}
+}
