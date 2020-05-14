13359732ea60dd288ed8d1183dfd92c2c39789f8
Andres Ramirez <andresramirezz@hotmail.com>
2020-05-13 22:06:23 -0400

    Add AccessLevel.java example
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/beginningJava8Fundamentals/classesAndObjects/AccessLevel.java
@@ -0,0 +1,34 @@
+package com.beginningJava8Fundamentals.classesAndObjects;
+
+/**
+ * @author andresr
+ * An AccessLevel Class with Class Members Having Different Access Levels
+ */
+
+public class AccessLevel {
+
+	private int v1 = 100;
+	int v2 = 200;
+	protected int v3 = 300; 
+	public int v4 = 400;
+	
+	private void m1() {
+		System.out.println("Inside m1():"); 
+		System.out.println("v1 = "+v1+ ", v2 = "+v2+ ", v3 = "+v3+ ", v4 = "+v4);
+	}
+	
+	void m2() {
+		System.out.println("Inside m2():"); 
+		System.out.println("v1 = "+v1+ ", v2 = "+v2+", v3 = "+v3+ ", v4 = "+v4);
+	}
+	
+	protected void m3() {
+		System.out.println("Inside m3():"); 
+		System.out.println("v1 = "+v1+ ", v2 = "+v2+", v3 = "+v3+ ", v4 = "+v4);
+	}
+	
+	public void m4() {
+		System.out.println("Inside m4():"); 
+		System.out.println("v1 = "+v1+ ", v2 = "+v2 +", v3 = "+v3 + ", v4 = "+v4);
+	} 
+}
