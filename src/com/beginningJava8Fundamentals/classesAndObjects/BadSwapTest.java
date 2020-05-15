4ea10bc4d4caed77f2388b378adeca8e17362a06
Andres Ramirez <andresramirezz@hotmail.com>
2020-05-14 22:47:41 -0400

    Add BadSwapTest.java example
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/beginningJava8Fundamentals/classesAndObjects/BadSwapTest.java
@@ -0,0 +1,31 @@
+package com.beginningJava8Fundamentals.classesAndObjects;
+
+/**
+ * @author andresr
+ * An Incorrect Attempt to Write a swap() Method to Swap Values of Two Primitive Types in Java
+ */
+
+public class BadSwapTest {
+
+	public static void swap(int x, int y) {
+		System.out.println("#2: x = " + x + ", y = " + y);
+		
+		int temp = x; 
+		x = y;
+		y = temp;
+	
+		System.out.println("#3: x = " + x + ", y = " + y);
+	}
+	
+	public static void main(String[] args) { 
+		int a = 19;
+		int b = 37;
+	
+		System.out.println("#1: a = " + a + ", b = " + b);
+		
+		// Call the swap() method to swap values of a and b 
+		BadSwapTest.swap(a, b);
+		
+		System.out.println("#4: a = " + a + ", b = " + b);
+	}
+}
