533d87c1c0cc13d4dadecda05bcf9bf895b384f1
Andres Ramirez <andresramirezz@hotmail.com>
2020-05-18 21:22:24 -0400

    Add DogTest.java example
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/beginningJava8Fundamentals/classesAndObjects/DogTest.java
@@ -0,0 +1,16 @@
+package com.beginningJava8Fundamentals.classesAndObjects;
+
+/**
+ * @author andresr
+ *
+ */
+public class DogTest {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		Dog d1 = new Dog();				// Uses Constructor #1
+		
+		Dog d2 = new Dog("Canis");		// Uses Constructor #2
+	}
+}
