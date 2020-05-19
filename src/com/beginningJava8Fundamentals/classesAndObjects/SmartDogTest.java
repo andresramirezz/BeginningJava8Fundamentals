90a97493ebcd2b2587c902acec3c427ff3948ac7
Andres Ramirez <andresramirezz@hotmail.com>
2020-05-18 22:06:27 -0400

    Add SmartDogTest.java example
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/beginningJava8Fundamentals/classesAndObjects/SmartDogTest.java
@@ -0,0 +1,36 @@
+package com.beginningJava8Fundamentals.classesAndObjects;
+
+/**
+ * @author andresr
+ *
+ */
+public class SmartDogTest {
+
+	public static void main(String[] args) {
+		// Create two SmartDog objects
+		
+		SmartDog sd1 = new SmartDog();
+		SmartDog sd2 = new SmartDog("Nova", 219.2);
+		
+		// Print details about the two dogs 
+		sd1.printDetails(); 
+		sd2.printDetails();
+		
+		// Make them bark 
+		sd1.bark(); 
+		sd2.bark();
+		
+		
+		// Change the name and price of Unknown dog 
+		sd1.setName("Opal");
+		sd1.setPrice(321.80);
+		
+		// Print details again 
+		sd1.printDetails(); 
+		sd2.printDetails();
+		
+		// Make them bark one more time 
+		sd1.bark();
+		sd2.bark();
+	}
+}
